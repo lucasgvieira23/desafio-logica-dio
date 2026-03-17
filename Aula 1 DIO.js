d8410d7d7515fbcf95a1fea17429816f78c2c073
lucasgvieira23 <lucasg.vieira23@gmail.com>
2026-03-17 10:15:03 -0300

    Desafio do classificador de nível de herói finalizado

---
+++ Aula 1 DIO.js
@@ -0,0 +1,8 @@
+const notificacao = "pokemon go diz"
+
+// saída
+console.log(notificacao + " Tem um novo pokemon na região ")
+console.log(notificacao + "Você foi derrotado por um líder")
+notificacao = "digimon go"
+
+console.log("O nome do jogo agora é" + notificacao)

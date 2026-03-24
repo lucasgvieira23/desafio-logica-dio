1caa681117976f2f31af9383e8f9af0125dd7d40
Lucas Gabriel Moura Vieira <lucasg.vieira23@gmail.com>
2026-03-24 20:31:11 -0300

    Delete Aula 1 DIO.js

--- Aula 1 DIO.js
+++
@@ -1,8 +0,0 @@
-const notificacao = "pokemon go diz"
-
-// saída
-console.log(notificacao + " Tem um novo pokemon na região ")
-console.log(notificacao + "Você foi derrotado por um líder")
-notificacao = "digimon go"
-
-console.log("O nome do jogo agora é" + notificacao)
